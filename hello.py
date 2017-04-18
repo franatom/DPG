5fc0bbcef682a7fa4962d5f3c5966166f616049d
franatom <tomas-franek@seznam.cz>
2017-04-18 11:38:49 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,5 +1,14 @@
-import sys
+iimport sys
 import multiprocessing as mp
 
 print("Python version %s " % str(sys.version))
 print("Cores          %s " % str(mp.cpu_count()))
+
+
+if True:
+  print("tady")
+else:
+  print("tady nikdy")
+  a = 0
+  b = 1
+  c = b / a
